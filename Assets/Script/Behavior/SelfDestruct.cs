e0546c580f37284ddbbc7e3a3d58060ab978c425
SardineFish <Sardine@live.cn>
2018-03-17 14:40:21 +0800

    add self desctruct

--- Assets/Script/Behavior/SelfDestruct.cs
+++ Assets/Script/Behavior/SelfDestruct.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class SelfDescruct : EntityBehavior<Entity>
 {
+    public float Time = 1;
 
-    // Use this for initialization
-    void Start()
+    float startTime;
+
+    private void OnEnable()
     {
-
+        startTime = UnityEngine.Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (UnityEngine.Time.time - startTime > this.Time)
+            GameObject.Destroy(gameObject);
     }
 }
