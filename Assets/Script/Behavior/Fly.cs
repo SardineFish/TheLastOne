6f481aa10ff704e0e4c8f22903aae37901f381f5
SardineFish <Sardine@live.cn>
2018-03-17 14:36:07 +0800

    update EntityBehavior & FlyBehavior

---
+++ Assets/Script/Behavior/Fly.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum FlyType
+{
+    Direction,
+    Destination,
+}
+public class Fly : EntityBehavior<Entity> {
+    public FlyType FlyType;
+    public float Speed;
+    public Vector3 Target;
+
+    private void FixedUpdate()
+    {
+        var dir = Target;
+        if (FlyType == FlyType.Destination)
+            dir = Target - transform.position;
+        transform.Translate(dir.normalized * Speed * Time.fixedDeltaTime, Space.World);
+    }
+}
