e3fcffb028d9b8f1cedf098f5f529f7c212ae71d
SardineFish <Sardine@live.cn>
2018-03-27 22:58:17 +0800

    feat: Move weapon to player root & set binding

---
+++ Assets/Editor/BindObjectEditor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Assets.Editor
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(BindObject))]
+    public class BindObjectEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            var bindObj = target as BindObject;
+            bindObj.BindTo = EditorGUILayout.ObjectField("Bind To: ", bindObj.BindTo, typeof(GameObject), true) as GameObject;
+            EditorGUILayout.Space();
+            bindObj.BindPosition = EditorGUILayout.Toggle("Bind Position: ", bindObj.BindPosition);
+            bindObj.BindRotation = EditorGUILayout.Toggle("Bind Rotation: ", bindObj.BindRotation);
+            //bindObj.BindScale = EditorGUILayout.Toggle("Bind Scale: ", bindObj.BindScale);
+            EditorGUILayout.Space();
+            if (GUILayout.Button("Set Relative"))
+            {
+                if(bindObj.BindTo)
+                {
+                    bindObj.relativePosition = bindObj.transform.position - bindObj.BindTo.transform.position;
+                    bindObj.relativeRotation = bindObj.transform.rotation * Quaternion.Inverse(bindObj.BindTo.transform.rotation);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Script/Component/BindObject.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class BindObject : MonoBehaviour
+{
+    public GameObject BindTo;
+    public bool BindPosition = true;
+    public bool BindRotation = true;
+    //public bool BindScale = true;
+    public Quaternion relativeRotation = Quaternion.identity;
+    public Vector3 relativePosition = Vector3.zero;
+    //public Vector3 relativeScale;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    [ExecuteInEditMode]
+    void Update()
+    {
+        if (BindTo)
+        {
+            if (BindPosition)
+                transform.position = BindTo.transform.position + relativePosition;
+            if (BindRotation)
+                transform.rotation = BindTo.transform.rotation * relativeRotation;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (BindTo)
+        {
+            if (BindPosition)
+                transform.position = BindTo.transform.position + relativePosition;
+            if (BindRotation)
+                transform.rotation = BindTo.transform.rotation * relativeRotation;
+        }
+        
+    }
+}
