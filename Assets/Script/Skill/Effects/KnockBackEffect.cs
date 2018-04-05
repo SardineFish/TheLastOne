a903948505f3f9a6889a70cd9169b29027e9aaf3
SardineFish <Sardine@live.cn>
2018-04-05 21:25:10 +0800

    feat(skill): Update knock back effect

--- Assets/Script/Skill/Effects/KnockBackEffect.cs
+++ Assets/Script/Skill/Effects/KnockBackEffect.cs
@@ -7,6 +7,7 @@ public class KnockBackEffect : SkillEffect
     public float Impulse;
     public override void ApplyEffect(SkillImpact impact, Entity target)
     {
-        target.GetComponent<CustomRigidBody>().AddForce((target.transform.position - impact.transform.position).ClipY().normalized * Impulse, ForceMode.Impulse);
+        //target.GetComponent<CustomRigidBody>().AddForce((target.transform.position - impact.transform.position).ClipY().normalized * Impulse, ForceMode.Impulse);
+        target.GetComponent<SkillController>().MovementSkill.KnockBack((target.transform.position - impact.transform.position).ClipY().normalized * Impulse);
     }
 }
