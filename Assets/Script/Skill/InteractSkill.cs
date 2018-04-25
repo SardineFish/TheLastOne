3bd931924084ad978ef6f717ea04a6cc4df55a7a
SardineFish <Sardine@live.cn>
2018-04-25 18:37:33 +0800

    feat(skill): Update interact skill

--- Assets/Script/Skill/InteractSkill.cs
+++ Assets/Script/Skill/InteractSkill.cs
@@ -1,8 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class InteractSkill : AnimationSkill
 {
+    List<InteractiveObject> objInRange = new List<InteractiveObject>();
 
     void Start()
     {
@@ -16,12 +19,32 @@ public class InteractSkill : AnimationSkill
     [EventListener(Entity.EVENT_TRIGGER_ENTER)]
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.gameObject.name + "In");
+        if (other.GetComponent<InteractiveObject>())
+            objInRange.Add(other.GetComponent<InteractiveObject>());
     }
 
     [EventListener(Entity.EVENT_TRIGGER_EXIT)]
     private void OnTriggerExit(Collider other)
     {
-        Debug.Log(other.gameObject.name + "Out");
+        if (other.GetComponent<InteractiveObject>())
+            objInRange.Remove(other.GetComponent<InteractiveObject>());
+    }
+
+    public override bool Activate()
+    {
+        return Activate(objInRange.OrderBy(obj => (obj.transform.position - Entity.transform.position).sqrMagnitude).FirstOrDefault());
+    }
+
+    public override bool Activate(Vector3 target)
+    {
+        return Activate();
+    }
+
+    public override bool Activate(Entity target)
+    {
+        if (!target || !(target is InteractiveObject))
+            return false;
+        new InteractMessage(Entity).Dispatch(target);
+        return true;
     }
 }
